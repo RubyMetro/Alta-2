110fa45fc8ba539fe256d6a63cdea36d12632751
christian thompson <cjthomp2005@outlook.com>
2021-11-26 00:27:36 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alta
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+            var token = File.ReadAllText("token.txt");
+            if (token.Length < 1)
+            {
+                Console.WriteLine("Enter Token");
+                File.WriteAllText("token.txt", Console.ReadLine());
+                token = File.ReadAllText("token.txt");
+            }
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
